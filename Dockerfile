affe4f2832d99bdb879a60f8cab1c2f1775545c7
jaivra <jaivra.info@gmail.com>
2022-08-29 14:05:01 +0200

    changed from client-server http approach to rpc

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,16 @@ FROM carlasim/carla:0.9.13
 
 USER root
 WORKDIR /app
+COPY requirements.txt ./requirements.txt
 RUN rm /etc/apt/sources.list.d/cuda.list \
     && apt update \
     && apt install -y --no-install-recommends python3-dev python3-setuptools python3-pip \
     && pip3 install --upgrade pip \
-    && pip3 install flask psutil
+    && pip install --no-cache-dir -r requirements.txt
+    #&& pip3 install zerorpc psutil
 ENV LC_ALL=C.UTF-8 
 ENV LANG=C.UTF-8
 COPY app.py app.py
 USER carla
-CMD python3 -m flask run --host=0.0.0.0
+CMD python3 app.py
+#CMD python3 -m flask run --host=0.0.0.0
--- app.py
+++ app.py
@@ -2,58 +2,62 @@ import json
 import signal
 
 import psutil
-from flask import Flask
-
-carla_simulator_proc = None
-carla_launch_script = ['/home/carla/CarlaUE4.sh', '-RenderOffScreen']
-
-app = Flask(__name__)
+import zerorpc
+from time import sleep
 
 
-def _carla_service_is_active():
-    global carla_simulator_proc
-    return carla_simulator_proc is not None and carla_simulator_proc.poll() is None
+class CarlaHandlerRPC:
+
+    def __init__(self) -> None:
+        self._carla_simulator_proc = None
+        self._carla_launch_script = ['/home/carla/CarlaUE4.sh', '-RenderOffScreen']
+
+    def _carla_service_is_active(self):
+        return self._carla_simulator_proc is not None and self._carla_simulator_proc.poll() is None
+
+    def _start_process(self):
+        self._carla_simulator_proc = psutil.Popen(self._carla_launch_script, shell=False)
+        print(self._carla_simulator_proc.pid)
+        while self._carla_simulator_proc.children(recursive=True) == 0:
+            ...
+
+    def _stop_process(self):
+        def kill_child_processes(parent_pid, sig=signal.SIGTERM):
+            try:
+                parent = psutil.Process(parent_pid)
+            except psutil.NoSuchProcess:
+                return
+            children = parent.children(recursive=True)
+            for process in children:
+                # app.logger.warning("====> " + str(process.pid))
+                process.terminate()
+                process.wait()
+
+        # app.logger.warning("====> " + str(carla_proc.pid))
+        kill_child_processes(self._carla_simulator_proc.pid)
+        self._carla_simulator_proc.terminate()
+        self._carla_simulator_proc.wait()
 
 
-def _start_process():
-    global carla_launch_script
-    return psutil.Popen(carla_launch_script, shell=False)
+    def reload_simulator(self):
+        if self._carla_service_is_active():
+            self._stop_process()
+        self._start_process()
+        res = {'result': True, 'pid': self._carla_simulator_proc.pid}
+        # return json.dumps(res)
+        return True
+
+    def close_simulator(self):
+        # app.logger.warning("====> " + str(_carla_service_is_active()))
+        if self._carla_service_is_active():
+            self._stop_process()
+        res = {'result': True}
+        # return json.dumps(res)
+        return True
 
 
-def _stop_process(carla_proc):
-    def kill_child_processes(parent_pid, sig=signal.SIGTERM):
-        try:
-            parent = psutil.Process(parent_pid)
-        except psutil.NoSuchProcess:
-            return
-        children = parent.children(recursive=True)
-        for process in children:
-            app.logger.warning("====> " + str(process.pid))
-            process.kill()
-            process.wait()
-
-    app.logger.warning("====> " + str(carla_proc.pid))
-    kill_child_processes(carla_proc.pid)
-    carla_proc.kill()
-    carla_proc.wait()
-
-
-@app.route("/reload_simulator")
-def reload_simulator():
-    global carla_simulator_proc
-    if _carla_service_is_active():
-        _stop_process(carla_simulator_proc)
-    carla_simulator_proc = _start_process()
-    res = {'result': True, 'pid': carla_simulator_proc.pid}
-    return json.dumps(res)
-
-
-@app.route("/finish_simulation")
-def finish_simulation():
-    global carla_simulator_proc
-    app.logger.warning("====> " + str(_carla_service_is_active()))
-
-    if _carla_service_is_active():
-        _stop_process(carla_simulator_proc)
-    res = {'result': True}
-    return json.dumps(res)
+if __name__ == '__main__':
+    s = zerorpc.Server(CarlaHandlerRPC())
+    s.bind('tcp://0.0.0.0:4242')
+    print('RPC server running')
+    s.run()
